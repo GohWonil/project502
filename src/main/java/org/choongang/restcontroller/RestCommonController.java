a99000aee86a6fafc67733f6c35703bb1446607e
kwi5124 <kwi5124@naver.com>
2023-12-29 15:12:48 +0900

    시큐티리 PC&MOBILE

---
+++ src/main/java/org/choongang/restcontroller/RestCommonController.java
@@ -0,0 +1,8 @@
+package org.choongang.restcontroller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice("org.choongang.restcontrollers")
+public class RestCommonController {
+
+}
