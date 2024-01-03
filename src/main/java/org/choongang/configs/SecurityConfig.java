3b96ad6157e461bc2d84cfc2896c6b552c0d75df
kwi5124 <kwi5124@naver.com>
2024-01-03 11:05:29 +0900

    인가 설정(접근통제)

--- src/main/java/org/choongang/configs/SecurityConfig.java
+++ src/main/java/org/choongang/configs/SecurityConfig.java
@@ -1,21 +1,15 @@
 package org.choongang.configs;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.choongang.member.service.LoginFailureHandler;
 import org.choongang.member.service.LoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.io.IOException;
 
 @Configuration
 public class SecurityConfig {
@@ -38,21 +32,29 @@ public class SecurityConfig {
     });
     /* 인증 설정 E - 로그인, 로그아웃 */
 
-    /* 인증 설정 S - 접근 통제 */
+    /* 인가 설정 S - 접근 통제 */
+    // hasAuthority(..) hasAnyAuthority(...), hasRole, hasAnyRole
+    // ROLE_롤명칭
+    // hasAuthority('ADMIN')
+    // ROLE_ADMIN -> hasAuthority('ROLE_ADMIN')
+    // hasRole('ADMIN')
     http.authorizeHttpRequests(c -> {
-      c.requestMatchers("/mypage/**")
-          .authenticated()
-          .requestMatchers("/admin/**")
-          .hasAnyAuthority("ADMIN", "MANAGER")
-          .anyRequest().permitAll();//그 외 모든 페이지는 모든 접근 가능
+      c.requestMatchers("/mypage/**").authenticated() // 회원 전용
+          .requestMatchers("/admin/**").hasAnyAuthority("ADMIN", "MANAGER")
+          .anyRequest().permitAll(); // 그외 모든 페이지는 모두 접근 가능
     });
 
     http.exceptionHandling(c -> {
-      c.authenticationEntryPoint(new AuthenticationEntryPoint() {
-        @Override
-        public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-
+      c.authenticationEntryPoint((req, res, e) -> {
+        String URL = req.getRequestURI();
+        if (URL.indexOf("/admin") != -1) { // 관리자 페이지
+          res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        } else { // 회원전용 페이지
+          res.sendRedirect(req.getContextPath() + "/member/login");
         }
+//      c.authenticationEntryPoint(new AuthenticationEntryPoint() {
+//        @Override
+//        public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
       });
     });
     /* 인가 설정 E - 접근 통제 */
