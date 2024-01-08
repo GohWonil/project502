73564d334cc1afd46704f5dc1d80f17904d4141e
kwi5124 <kwi5124@naver.com>
2024-01-08 14:57:12 +0900

    파일 업로드, 다운로드

---
+++ src/main/resources/static/common/js/fileManager.js
@@ -0,0 +1,71 @@
+var commonLib = commonLib || {};
+/**
+ * 1. 파일 업로드
+ *
+ */
+commonLib.fileManager = {
+  /**
+   * 파일 업로드 처리
+   *
+   */
+  upload(files) {
+    try {
+      if(!files || files.length == 0){
+        throw new Error("업로드 파일 선택")
+      }
+      //gid
+      const gidEl = document.querySelector("[name='gid']");
+      if(!gidEl || gidEl.value.trim()){
+        throw new Error("잘못된 접근입니다.")
+      }
+
+      const gid = gdiEl.value.trim();
+      const formData = new FormData();//기본 Content-Type: multipart/form-data
+      formData.append("gid", gid);
+      for(const file of files) {
+         formData.append("file", file);
+      }
+
+      const { ajaxLoad } = commonLib;
+      ajaxLoad("POST", "/api/file", formData, "json")
+          .then(res => { // 요청 성공시
+            if (res && res.success) { // 파일 업로드 성공시
+
+              if (typeof parent.callbackFileUpload == 'function') {
+                parent.callbackFileUpload(res.data);
+              }
+
+            } else { // 파일 업로드 실패시
+              if (res) alert(res.message);
+            }
+          })
+          .catch(err => console.error(err));
+
+    } catch (err){
+      alert(err.message);
+      console.error(err);
+    }
+  }
+};
+
+
+// 이벤트 처리
+window.addEventListener("DOMContentLoaded", function() {
+  const uploadFiles = document.getElementsByClassName("upload_files");
+
+  const fileEl = document.createElement("input");
+  fileEl.type="file";
+  fileEl.multiple = true; //여러개 파일을 선택 가능하게
+
+  //파일 업로드 버튼 클릭 처리 -> 파일 탐색기 열기
+  for (const el of uploadFiles) {
+    el.addEventListener("click", function() {
+      fileEl.click();
+    });
+  }
+
+  //파일 선택시 이벤트 처리
+  fileEl.addEventListener("chage", function (e){
+    commonLib.fileManager.upload(e.target.files);
+  });
+});
